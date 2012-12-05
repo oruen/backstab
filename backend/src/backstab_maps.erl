39830e266fd2fa27fe5b285cfcf15bfef23354b3
Nick Recobra <oruen@undev.ru>
2012-12-05 17:47:38 +0400

    First take on random maps.

--- backend/src/backstab_maps.erl
+++ backend/src/backstab_maps.erl
@@ -1,5 +1,5 @@
 -module(backstab_maps).
--export([load/1]).
+-export([load/1, random/0]).
 -include("backstab.hrl").
 
 load(_MapId) ->
@@ -16,3 +16,46 @@ load(_MapId) ->
          #route{from = <<"2">>, to = <<"3">>},
          #route{from = <<"3">>, to = <<"4">>}]}
    ].
+
+random() ->
+  UserPlanets = [random_planet(<<"1">>),
+                 random_planet(<<"1">>),
+                 random_planet(<<"2">>),
+                 random_planet(<<"2">>)],
+  PlanetsNum = random:uniform(10) + 8,
+  Planets = generate_planets(PlanetsNum),
+  [{planets, UserPlanets ++ Planets}].
+
+generate_planets(Num) ->
+  generate_planets(Num, []).
+
+generate_planets(0, Acc) ->
+  Acc;
+generate_planets(Num, Acc) ->
+  generate_planets(Num - 1, [random_planet() | Acc]).
+
+random_planet() ->
+  UserId = random_user(),
+  random_planet(UserId).
+
+random_planet(UserId) ->
+  Uid = uuid:to_string(uuid:v4()),
+  Capacity = random:uniform(7) * 5 + 15,
+  random_planet(UserId, Uid, Capacity).
+
+random_planet(undefined, Uid, Capacity) ->
+  #planet{id = Uid, capacity = Capacity, type = <<"ground">>};
+random_planet(UserId, Uid, Capacity) ->
+  Quantity = random:uniform(3) * 5,
+  #planet{id = Uid, capacity = Capacity, type = <<"ground">>, user_id = UserId, quantity = Quantity}.
+
+random_user() ->
+  random_user(random:uniform(15)).
+
+random_user(Seed) when Seed < 3 ->
+  <<"1">>;
+random_user(Seed) when Seed > 13 ->
+  <<"2">>;
+random_user(_) ->
+  undefined.
+
